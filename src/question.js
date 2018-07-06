89cdace86f12a2eec84d4cf64c6b4087ee7679f5
BarkerAW83 <BarkerAW@gmail.com>
2018-07-06 18:58:27 -0400

    "Buttons populating currentPlayer names"

--- src/question.js
+++ src/question.js
@@ -5,7 +5,9 @@ class Question extends React.Component{
     super(props);
     this.state = {
       currentPlayerA: null,
-      currentPlayerB: null
+      currentPlayerB: null,
+      nameA: null,
+      nameB: null
     }
     this.getRandomPlayers = this.getRandomPlayers.bind(this);
     }
@@ -15,12 +17,17 @@ class Question extends React.Component{
       }
     }
     getRandomPlayers(){
-      this.setState({currentPlayerA: this.props.players[(Math.floor(Math.random() * (this.props.rosterLength - 0 + 1) + 0))], currentPlayerB: this.props.players[(Math.floor(Math.random() * (this.props.rosterLength - 0 + 1) + 0))]}, ()=>{console.log(this.state.currentPlayerA, this.state.currentPlayerB)})
+      this.setState({currentPlayerA: this.props.players[(Math.floor(Math.random() * (this.props.rosterLength - 0 + 1) + 0))], 
+                     currentPlayerB: this.props.players[(Math.floor(Math.random() * (this.props.rosterLength - 0 + 1) + 0))]}, 
+                    ()=>{this.setState({nameA: this.state.currentPlayerA.firstName, nameB: this.state.currentPlayerB.firstName}, 
+                    ()=>{console.log(this.state.currentPlayerA, this.state.currentPlayerB)})})
     }
+
     render(){
       return(
         <div>
-
+        <button>{this.state.nameA}</button>
+        <button>{this.state.nameB}</button>
         </div>
         );
   }
