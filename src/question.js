bc4b9cc47886fdcb8118926fc5d7fc57f7b4d02c
BarkerAW83 <BarkerAW@gmail.com>
2018-07-16 22:21:07 -0400

    "randomPlayer function implemented w/ stats"

--- src/question.js
+++ src/question.js
@@ -6,9 +6,15 @@ class Question extends React.Component{
     this.state = {
       currentPlayerA: null,
       currentPlayerB: null,
+      randomPlayer: null,
+      randomPlayerVar: null,
       propsLoaded: false
     }
     this.getRandomPlayers = this.getRandomPlayers.bind(this);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    }
+    componentDidMount(){
+      this.setState({randomPlayerVar: Math.floor(Math.random() * (1 - 0 + 1) + 0)})
     }
     componentDidUpdate(){
       if (this.props.rosterLength !== null && this.state.currentPlayerA === null){
@@ -16,27 +22,40 @@ class Question extends React.Component{
       }
     }
     getRandomPlayers(){
-      this.setState({currentPlayerA: this.props.players[(Math.floor(Math.random() * (this.props.rosterLength - 0 + 1) + 0))], 
-                    currentPlayerB: this.props.players[(Math.floor(Math.random() * (this.props.rosterLength - 0 + 1) + 0))],
-                    propsLoaded: true}, 
-                    ()=>console.log(this.state.currentPlayerA, this.state.currentPlayerB))
+      var number = (Math.floor(Math.random() * (1 - 0 + 1) + 0));
+      var randomA = (Math.floor(Math.random() * (this.props.rosterLength - 0 + 1) + 0));
+      var randomB = (Math.floor(Math.random() * (this.props.rosterLength - 0 + 1) + 0));
+      this.setState({currentPlayerA: this.props.players[randomA], 
+                    currentPlayerB: this.props.players[randomB],
+                    propsLoaded: true,
+                    // randomPlayer: ((this.state.randomPlayerVar) ? this.state.currentPlayerA : this.state.currentPlayerB)}, 
+                    randomPlayer: ((this.state.randomPlayerVar > 0) ? this.props.players[randomA] : this.props.players[randomB])}, 
+                    ()=>console.log(this.state.currentPlayerA, this.state.currentPlayerB, 'RANDOM PLAYER IS, ', this.state.randomPlayer))
+    }
+    handleButtonClick(key, event){
+      console.log(key, ': IS THE KEY')
     }
     render(){
       var playerA;
       var playerB;
+      var idA;
+      var idB;    
 
       if (this.state.propsLoaded){
         playerA = this.state.currentPlayerA.lastName,
+        idA = this.state.currentPlayerA._id,
         playerB = this.state.currentPlayerB.lastName
+        idB = this.state.currentPlayerB._id
       }
-      else{
-        playerA = null,
-        playerB = null
-      }
+      // else{
+      //   playerA = null,
+      //   playerB = null
+      // }
       return(
         <div>
-        <div><button>{playerA}</button></div>
-        <div><button>{playerB}</button></div>
+        <div><button onClick={()=>this.handleButtonClick(idA)}>{playerA}</button></div>
+        <div><button onClick={()=>this.handleButtonClick(idB)}>{playerB}</button></div>
+        <h2></h2>
         </div>
         );
   }
